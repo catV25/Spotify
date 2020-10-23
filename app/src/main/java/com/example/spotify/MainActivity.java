260b921428318c57582915eeec0c43d9f8d70a1d
3AIMAQ08 <3AIMAQ08@fundacaonokia.org>
2020-10-23 12:19:57 -0400

    first commit

--- app/src/main/java/com/example/spotify/MainActivity.java
+++ app/src/main/java/com/example/spotify/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.spotify;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView coverImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //coverImage findViewById(R.id.vImage);
     }
 }
